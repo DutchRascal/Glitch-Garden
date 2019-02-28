6f8c923f896b934bd64d6e75261407e665539bf1
DutchRascal <dutchrascal@fritz.box>
2019-02-28 15:44:04 +0100

    Initial commit

---
+++ Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+    int currentSceneIndex;
+
+    [SerializeField] int timeToWait = 3;
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
