3da3fd028bc1f2c565ba7d8da964e348fe91dded
Rascal <andre.boevink@me.com>
2019-03-04 10:48:43 +0100

    136

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,13 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
+    [SerializeField] float damage = 50f;
 
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
     }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+        health.DealDamage(damage);
+    }
+
 }
