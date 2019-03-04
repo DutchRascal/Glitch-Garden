859d2a0fe915b70c1001d8f154343596d9d5cc30
Rascal <andre.boevink@me.com>
2019-03-04 11:13:06 +0100

    137

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,14 +7,22 @@ public class Health : MonoBehaviour
 {
 
     [SerializeField] float health = 100f;
+    [SerializeField] GameObject deathVFX;
 
     public void DealDamage(float damage)
     {
         health -= damage;
         if (health <= 0)
         {
+            TriggerDeathVFX();
             Destroy(gameObject);
         }
     }
 
+    private void TriggerDeathVFX()
+    {
+        if (!deathVFX) { return; }
+        GameObject deathVFXObject = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(deathVFXObject, 1f);
+    }
 }
