835fd7e98b65ad3ad07a3a00cbe51a5042515dda
Rascal <andre.boevink@me.com>
2019-03-08 14:56:44 +0100

    150

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,4 +1,5 @@
 ﻿#pragma warning disable 649
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +8,50 @@ public class Shooter : MonoBehaviour
 {
 
     [SerializeField] GameObject projectile, gun;
+    AttackerSpawner myLaneSpawner;
 
 #pragma warning restore  649
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool IsCloseEnough = Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon;
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("Shoot");
+            //TODO
+        }
+        else
+        {
+            Debug.Log("Sit and Wait");
+            //TODO
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        return true;
+    }
 
     public void Fire()
     {
